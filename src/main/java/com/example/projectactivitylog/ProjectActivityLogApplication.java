346f7755745030e0c147d31bb7a5e7bb381a8f58
Tomas Duchek <neb7pxuk@duchic.anonaddy.com>
2023-06-20 17:41:17 +0200

    Initial commit

---
+++ src/main/java/com/example/projectactivitylog/ProjectActivityLogApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectactivitylog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectActivityLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectActivityLogApplication.class, args);
+	}
+
+}
